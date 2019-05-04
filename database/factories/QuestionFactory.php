5e6fc29aa50f7a2503477899d161fae5d749ffd3
radiatenoor <radiate126@gmail.com>
2019-05-04 19:45:31 +0600

    faker yajra

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'category_id'=>rand(1,3),
+        'user_id'=>3,
+        'title'=>$faker->sentence,
+        'description'=>$faker->text,
+        'status'=>1,
+        'created_at'=>date('Y-m-d h:i:s'),
+        'updated_at'=>date('Y-m-d h:i:s'),
+    ];
+});
